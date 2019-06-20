10b9d30bd72607b5f0e521973ff828834932ed73
Shunsuke Osaka <osaka@ialab.cs.tsukuba.ac.jp>
2019-06-20 13:30:24 +0900

    first commit(main source and layout)

---
+++ app/src/main/java/com/example/controllertest/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.controllertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mtv_state;
+
+    Button mb_go_forward;
+    Button mb_lotate_left;
+    Button mb_lotate_right;
+    Button mb_go_backward;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mtv_state = (TextView)findViewById(R.id.text_view_state);
+
+        mb_go_forward = (Button)findViewById(R.id.button_go_forward);
+        mb_lotate_left = (Button)findViewById(R.id.button_lotate_left);
+        mb_lotate_right = (Button)findViewById(R.id.button_lotate_right);
+        mb_go_backward = (Button)findViewById(R.id.button_go_backward);
+
+        mb_go_forward.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // 指がタッチした時の処理を記述
+                    mtv_state.setText("前進！");
+                    mb_lotate_left.setEnabled(false);
+                    mb_lotate_right.setEnabled(false);
+                    mb_go_backward.setEnabled(false);
+                } else if(event.getAction() == MotionEvent.ACTION_UP) {
+                    // タッチした指が離れた時の処理を記述
+                    mtv_state.setText("停止！");
+                    mb_lotate_left.setEnabled(true);
+                    mb_lotate_right.setEnabled(true);
+                    mb_go_backward.setEnabled(true);
+                }
+                return false;
+            }
+        });
+
+        mb_lotate_left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // 指がタッチした時の処理を記述
+                    mtv_state.setText("左回転！");
+                    mb_go_forward.setEnabled(false);
+                    mb_lotate_right.setEnabled(false);
+                    mb_go_backward.setEnabled(false);
+                } else if(event.getAction() == MotionEvent.ACTION_UP) {
+                    // タッチした指が離れた時の処理を記述
+                    mtv_state.setText("停止！");
+                    mb_go_forward.setEnabled(true);
+                    mb_lotate_right.setEnabled(true);
+                    mb_go_backward.setEnabled(true);
+                }
+                return false;
+            }
+        });
+
+        mb_lotate_right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // 指がタッチした時の処理を記述
+                    mtv_state.setText("右回転！");
+                    mb_go_forward.setEnabled(false);
+                    mb_lotate_left.setEnabled(false);
+                    mb_go_backward.setEnabled(false);
+                } else if(event.getAction() == MotionEvent.ACTION_UP) {
+                    // タッチした指が離れた時の処理を記述
+                    mtv_state.setText("停止！");
+                    mb_go_forward.setEnabled(true);
+                    mb_lotate_left.setEnabled(true);
+                    mb_go_backward.setEnabled(true);
+                }
+                return false;
+            }
+        });
+
+        mb_go_backward.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // 指がタッチした時の処理を記述
+                    mtv_state.setText("後退！");
+                    mb_go_forward.setEnabled(false);
+                    mb_lotate_left.setEnabled(false);
+                    mb_lotate_right.setEnabled(false);
+                } else if(event.getAction() == MotionEvent.ACTION_UP) {
+                    // タッチした指が離れた時の処理を記述
+                    mtv_state.setText("停止！");
+                    mb_go_forward.setEnabled(true);
+                    mb_lotate_left.setEnabled(true);
+                    mb_lotate_right.setEnabled(true);
+                }
+                return false;
+            }
+        });
+    }
+}
